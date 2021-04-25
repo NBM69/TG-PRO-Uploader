34512758b77aa6144be2a9e61d2a53f0bcaeff5d
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-04-25 19:27:26 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,5 @@
 from os.path import join as os_path_join
-from pyrogram import Client, Message, MessageHandler, Filters, CallbackQueryHandler
+from pyrogram import Client, message, MessageHandler, Filters, CallbackQueryHandler
 from bot import CONFIG, COMMAND, LOCAL, LOGGER, STATUS
 from bot.handlers import *
 import asyncio
